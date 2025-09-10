4ecb1e6543bb79d3d9a7d1df5dd828be4ee7bd8f
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-10 23:50:28 +0200

    feat: html form structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,50 @@ function App() {
 					<div className="col-12">
 						<h1>React post form</h1>
 					</div>
+					<div className="col-12">
+						<form>
+							<div className="row">
+								<div className="col-12 col-lg-6">
+									<label htmlFor="" className="form-label">
+										Autore
+									</label>
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Autore"
+									/>
+								</div>
+								<div className="col-12 col-lg-6">
+									<label htmlFor="" className="form-label">
+										Titolo
+									</label>
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Titolo"
+									/>
+								</div>
+								<div className="col-12 col-lg-6">
+									<label htmlFor="" className="form-label">
+										Autore
+									</label>
+									<input type="checkbox" className="form-check-input" />
+								</div>
+								<div className="col-12 col-lg-6">
+									<label htmlFor="" className="form-label">
+										Testo
+									</label>
+									<textarea
+										name=""
+										id=""
+										rows="4"
+										className="form-control"
+										placeholder="Testo"
+									></textarea>
+								</div>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</>
