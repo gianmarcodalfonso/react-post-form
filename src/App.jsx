1d4ee18205f652d47262caf8e8587db9363eda9c
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-10 23:42:12 +0200

    build: add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import { useState } from "react";
 function App() {
-	return <></>;
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<h1>React post form</h1>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
